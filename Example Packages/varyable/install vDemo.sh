5b8a893692e69f18936099781acc8dfb1943e6e4
lezzetli-mozaik18 <155469524+lezzetli-mozaik18@users.noreply.github.com>
2026-06-11 14:14:30 +0300

    Create install vDemo.sh

---
+++ Example Packages/varyable/install vDemo.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+echo "install.sh"
+echo
+echo "  varyable Demo"
+groups | grep -e "sudo"
+echo 
+echo 'sudo mkdir "/opt/varyable"'
+sudo mkdir "/opt/varyable"
+echo 'sudo touch "usr/local/bin/varyable"'
+sudo touch "/usr/local/bin/varyable"
+sudo cat << 'EOF' > /usr/local/bin/varyable
+#!/bin/bash
+
+case "$1" in
+
+-new) 
+if [ -f "/opt/varyable/$2" ]; then 
+echo "fatal: already exists"
+else
+sudo touch "/opt/varyable/$2"
+fi
+;;
+
+-del)
+if [ -f "/opt/varyable/$2" ]; then
+sudo rm -rf "/opt/varyable/$2"
+else
+echo "fatal: not found"
+fi
+;;
+
+-set)
+if [ -f "/opt/varyable/$2" ]; then
+read -p "set $2 to: " set
+echo "$set" | sudo tee "/opt/varyable/$2" > /dev/null
+else
+echo "fatal: not found"
+fi
+;;
+
+-ls)
+ls /opt/varyable
+;;
+
+-get)
+if [ -f "/opt/varyable/$2" ]; then
+sudo cat "/opt/varyable/$2"
+else
+echo "fatal: not found"
+fi
+;;
+
+*)
+echo
+echo "-new"
+echo "-del"
+echo "-ls"
+echo "-set"
+echo "-get"
+echo
+;;
+esac
+EOF
+echo "made varyable"
+sudo chmod +x "/usr/local/bin/varyable"
+echo "made executable"
+echo "done"
+
+exit 0
