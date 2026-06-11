ecd2f3605906979ae0b46a5ff78ec2e6fd58f9ed
lezzetli-mozaik18 <155469524+lezzetli-mozaik18@users.noreply.github.com>
2026-06-11 15:01:53 +0300

    Create newuser.sh

---
+++ Example Packages/the “most example” packages/newuser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "newuser.sh"
+read -p "This is a tool that replaces 'sudo adduser' with 'newuser', as sudo adduser is frequently used to create new users. Please press enter to continue installing or CTRL+C to terminate the process." null
+
+if [ -d "/usr/local/bin" ]; then
+    # Create the binary file
+    sudo touch "/usr/local/bin/newuser"
+    
+    # Write the script content with a valid shebang securely
+    sudo cat << 'EOF' > "/usr/local/bin/newuser"
+#!/bin/bash
+sudo adduser "$1"
+EOF
+
+    # Grant execution permissions to the run tool itself
+    sudo chmod +x "/usr/local/bin/newuser"
+    echo "Installation complete: 'newuser' is ready to use."
+else
+    echo "fatal: /usr/local/bin not found"
+    exit 1
+fi
+
+exit 0
